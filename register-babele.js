9ca992d7935abb8d818f5adf6a1f2c443a65ad24
Nibgnas Prunuslee <113924215+nibgnas@users.noreply.github.com>
2022-11-14 21:54:35 +0900

    Add files via upload

--- register-babele.js
+++ register-babele.js
@@ -3,7 +3,7 @@ Hooks.once('init', () => {
 	if(typeof Babele !== 'undefined') {
 		
 		Babele.get().register({
-			module: 'pf2eko',
+			module: 'pf2e-ko',
 			lang: 'ko',
 			dir: 'compendium'
 		});
